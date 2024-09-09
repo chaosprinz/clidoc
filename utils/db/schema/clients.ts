9d030ec8e7efaf3773de633c73f0525f7cd41d93
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-09-09 17:12:24 +0200

    using only english terms in coe-related text

--- utils/db/schema/clients.ts
+++ utils/db/schema/clients.ts
@@ -1,10 +1,10 @@
 import { serial, mysqlTable, varchar } from 'drizzle-orm/mysql-core'
 
-export const klienten = mysqlTable('Klienten', {
+export const clients = mysqlTable('Klienten', {
   id: serial('id').primaryKey(),
   vorname: varchar('vorname', { length: 255 }).notNull(),
   familienname: varchar('familienname', { length: 255 }).notNull(),
 })
 
-export type Klient = typeof klienten.$inferSelect
-export type NewKlient = typeof klienten.$inferInsert
+export type Client = typeof clients.$inferSelect
+export type NewClient = typeof clients.$inferInsert
