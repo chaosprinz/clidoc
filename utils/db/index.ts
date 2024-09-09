175bf153ce881687ec7797b12515413fadaf85cb
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-09-09 16:59:52 +0200

    fix up database-connection

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -1,14 +1,35 @@
 import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2/promise'
 
-const createDb = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'clidoc_dev',
-    user: process.env.DB_USER || 'clidoc',
-    password: process.env.DB_PASS,
-  })
-  return drizzle(connection)
+let db
+let connection: any
+
+async function connect() {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'clidoc',
+      database: process.env.DB_NAME || 'clidoc_dev',
+      password: process.env.DB_PASS,
+    })
+  }
+  db = drizzle(connection)
+
+  function shutdown() {
+    if (connection) {
+      console.log('Shutting down db')
+      connection
+        .end()
+        .then(() => console.log('DB shutdown'))
+        .catch(console.error)
+        .finally(() => process.exit(0))
+    }
+  }
+
+  process.on('SIGINT', shutdown)
+  process.on('SIGTERM', shutdown)
+
+  return db
 }
 
-export const db = createDb()
+export { connect, connection }
