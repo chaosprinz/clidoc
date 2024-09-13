dba17401312f20130e2a35b6a6845fea7658a38e
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-09-13 11:54:30 +0200

    simplify db-export by using tl-await

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -1,35 +1,12 @@
 import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2/promise'
+import * as schema from '@/utils/db/schema'
 
-let db
-let connection: any
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'clidoc',
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME || 'clidoc_dev',
+})
 
-async function connect() {
-  if (!connection) {
-    connection = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'clidoc',
-      database: process.env.DB_NAME || 'clidoc_dev',
-      password: process.env.DB_PASS,
-    })
-  }
-  db = drizzle(connection)
-
-  function shutdown() {
-    if (connection) {
-      console.log('Shutting down db')
-      connection
-        .end()
-        .then(() => console.log('DB shutdown'))
-        .catch(console.error)
-        .finally(() => process.exit(0))
-    }
-  }
-
-  process.on('SIGINT', shutdown)
-  process.on('SIGTERM', shutdown)
-
-  return db
-}
-
-export { connect, connection }
+export const db = drizzle(connection, { schema, mode: 'default' })
